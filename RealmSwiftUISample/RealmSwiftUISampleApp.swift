87ab772a9d49d4cfa42bd8fb57d2304b2d8a9003
LeoAndo <reo.ando@sun-asterisk.com>
2022-05-10 23:17:42 +0900

    add swiftUI template project.

---
+++ RealmSwiftUISample/RealmSwiftUISampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmSwiftUISampleApp.swift
+//  RealmSwiftUISample
+//
+//  Created by LeoAndo on 2022/05/10.
+//
+
+import SwiftUI
+
+@main
+struct RealmSwiftUISampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
